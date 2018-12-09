be12b405687c933d5a9caa53df3809d26a69d483
Marcin Sulewski <Rivenatti@gmail.com>
2018-12-09 12:50:15 +0100

    Update delete.js

--- client/src/api/delete.js
+++ client/src/api/delete.js
@@ -2,12 +2,13 @@ import axios from "axios";
 
 // Delete user api request
 const deleteUser = (dispatch, userId, history) => {
+  console.log(userId);
   axios
     .delete(`/delete/${userId}`)
     .then(res => {
       history.push("/delete");
     })
-    .catch(error => console.log(error.response.data.error));
+    .catch(error => console.log(error.response));
 };
 
 export default { deleteUser };
